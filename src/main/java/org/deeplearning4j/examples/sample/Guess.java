66efefbcee6f697a684f49e6339d420d27051992
Jonathan Chang, Chun-yien <ccy@musicapoetica.org>
2020-11-20 05:16:58 +0800

    add properties; minor improve.

--- src/main/java/org/deeplearning4j/examples/sample/Guess.java
+++ src/main/java/org/deeplearning4j/examples/sample/Guess.java
@@ -17,17 +17,14 @@ package org.deeplearning4j.examples.sample;
 
 import java.awt.Color;
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.AbstractMap.SimpleEntry;
+import java.io.InputStream;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
@@ -41,89 +38,76 @@ import processing.core.PImage;
  */
 public class Guess extends TestModel {
 
-    Map<String, Integer> inputs;
-    Iterator<SimpleEntry<PImage, Integer>> source;
+   Properties inputs = new Properties();
+   Iterator<Entry<String, Integer>> source;
 
-    @Override
-    public void setup() {
+   static final String INPUT_LIST = "/input_list.properties";
 
-        colorMode(RGB);
-        frameRate(1.5f);
-        strokeWeight(0.1f);
-        stroke(100f);
-        textSize(dotSize * 4);
+   @Override
+   public void setup() {
 
-        inputs = new HashMap<>();
-        inputs.put("num_0.png", 0);
-        inputs.put("num_1.png", 1);
-        inputs.put("num_2.png", 2);
-        inputs.put("num_3.png", 3);
-        inputs.put("num_4.png", 4);
-        inputs.put("num_5.png", 5);
-        inputs.put("num_6.png", 6);
-        inputs.put("num_7.png", 7);
-        inputs.put("num_8.png", 8);
-        inputs.put("num_9.png", 9);
-        try {
-            model = MultiLayerNetwork.load(new File(path), false);
-            System.out.println(model.summary());
-            source = inputs.entrySet().stream()
-                    .map(e -> new SimpleEntry<>(loadImage(filenameToURI(e.getKey()).getPath()), e.getValue()))
-                    .collect(Collectors.toList())
-                    .iterator();
-        } catch (Exception ex) {
-            Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+      colorMode(RGB);
+      frameRate(1.5f);
+      strokeWeight(0.1f);
+      stroke(100f);
+      textSize(dotSize * 4);
 
-    public static URI filenameToURI(String filename) {
+      try ( InputStream input_list = this.getClass()
+              .getResourceAsStream(INPUT_LIST)) {
+         model = MultiLayerNetwork.load(new File(model_path), false);
+         inputs.load(input_list);
+      } catch (Exception ex) {
+         Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      System.out.println(model.summary());
+      source = inputs.entrySet().stream()
+              .map(e -> Map.entry(e.getKey().toString(), Integer.valueOf(e.getValue().toString().trim())))
+              .iterator();
+   }
 
-        try {
-            return Guess.class.getResource("/numbers/" + filename).toURI();
-        } catch (URISyntaxException ex) {
-            Logger.getLogger(Guess.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
+   public String getImagePath(String filename) {
 
-    @Override
-    public void draw() {
+      return this.getClass().getResource("/numbers/" + filename).getPath();
+   }
 
-        if (source.hasNext()) {
-            background(0);
-            Entry<PImage, Integer> input = source.next();
-            int[] data = input.getKey().pixels;
-            float[] data0 = new float[data.length];
-            for (int i = 0; i < data.length; i++) {
-                data0[i] = (float) (data[i] & 0x00ffff) / 0xffff;
+   @Override
+   public void draw() {
+
+      if (source.hasNext()) {
+         background(0);
+         Entry<String, Integer> input = source.next();
+         PImage image = loadImage(getImagePath(input.getKey()));
+         float[] data0 = new float[28 * 28];
+         for (int y = 0; y < 28; y++) {
+            for (int x = 0; x < 28; x++) {
+               int index = y * 28 + x;
+               int point = image.get(x, y);
+               data0[index] = (float) (point & 0x00ffff) / 0xffff;
+               fill(point);
+               rect(x * dotSize, y * dotSize,
+                       (x + 1) * dotSize, (y + 1) * dotSize);
             }
-            for (int y = 0; y < 28; y++) {
-                for (int x = 0; x < 28; x++) {
-                    int index = y * 28 + x;
-                    fill(data0[index] * 255);
-                    rect(x * dotSize, y * dotSize,
-                            (x + 1) * dotSize, (y + 1) * dotSize);
-                }
-            }
-            float[] array = model.activate(new NDArray(data0), Layer.TrainingMode.TEST).toFloatVector();
-            StringBuilder result = new StringBuilder();
-            int guess = IntStream.range(0, array.length)
-                    .peek(i -> result.append(String.format(" %d=%.3f ", i, array[i])))
-                    .boxed()
-                    .sorted(Comparator.comparing(i -> array[i], Comparator.reverseOrder()))
-                    .findFirst().get();
-            int answer = input.getValue();
-            fill(answer == guess ? 255 : Color.RED.getRGB());
-            text(answer, dotSize, dotSize * 4);
-            System.out.printf("%s [%s]\n",
-                    result,
-                    "A=" + answer + (answer == guess ? "" : ",G=" + guess));
-        } else {
-            noLoop();
-        }
-    }
+         }
+         float[] array = model.activate(new NDArray(data0), Layer.TrainingMode.TEST).toFloatVector();
+         StringBuilder result = new StringBuilder();
+         int guess = IntStream.range(0, array.length)
+                 .peek(i -> result.append(String.format(" %d=%.3f ", i, array[i])))
+                 .boxed()
+                 .sorted(Comparator.comparing(i -> array[i], Comparator.reverseOrder()))
+                 .findFirst().get();
+         int answer = input.getValue();
+         fill(answer == guess ? 255 : Color.RED.getRGB());
+         text(answer, dotSize, dotSize * 4);
+         System.out.printf("%s [%s]\n",
+                 result,
+                 "A=" + answer + (answer == guess ? "" : ",G=" + guess));
+      } else {
+         this.exit();
+      }
+   }
 
-    public static void main(String[] args) {
-        PApplet.main(Guess.class.getName());
-    }
+   public static void main(String[] args) {
+
+      PApplet.main(Guess.class.getName());
+   }
 }
--- src/main/java/org/deeplearning4j/examples/sample/TestModel.java
+++ src/main/java/org/deeplearning4j/examples/sample/TestModel.java
@@ -21,74 +21,73 @@ import processing.core.PApplet;
  */
 public class TestModel extends PApplet {
 
-    int batchSize = 1; // Test batch size;
-    int dotSize = 10;
-    MultiLayerNetwork model;
-    DataSetIterator mnistTest;
-    String path = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "lenetmnist.zip");
+   int batchSize = 1; // Test batch size;
+   int dotSize = 10;
+   MultiLayerNetwork model;
+   DataSetIterator mnistTest;
+   String model_path = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "lenetmnist.zip");
 
-    @Override
-    public void settings() {
+   @Override
+   public void settings() {
 
-        size(dotSize * 28, dotSize * 28);
-    }
+      size(dotSize * 28, dotSize * 28);
+   }
 
-    @Override
-    public void setup() {
+   @Override
+   public void setup() {
 
-        colorMode(RGB);
-        frameRate(1.5f);
-        strokeWeight(0.1f);
-        stroke(100f);
-        textSize(dotSize * 4);
-        try {
-            mnistTest = new MnistDataSetIterator(batchSize, false, 12345);
-            model = MultiLayerNetwork.load(new File(path), false);
-            System.out.println(model.summary());
-        } catch (IOException ex) {
-            Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+      colorMode(RGB);
+      frameRate(1.5f);
+      strokeWeight(0.1f);
+      stroke(100f);
+      textSize(dotSize * 4);
+      try {
+         mnistTest = new MnistDataSetIterator(batchSize, false, 12345);
+         model = MultiLayerNetwork.load(new File(model_path), false);
+         System.out.println(model.summary());
+      } catch (IOException ex) {
+         Logger.getLogger(TestModel.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
 
-    @Override
-    public void draw() {
+   @Override
+   public void draw() {
 
-        if (mnistTest.hasNext()) {
-            background(0);
-            DataSet input = mnistTest.next();
-            float[] data0 = input.asList().get(0).getFeatures().toFloatVector();
-            for (int y = 0; y < 28; y++) {
-                for (int x = 0; x < 28; x++) {
-                    int index = y * 28 + x;
-                    float point = data0[index] * 255;
-                    fill(point);
-                    rect(x * dotSize, y * dotSize,
-                            (x + 1) * dotSize, (y + 1) * dotSize);
-                }
+      if (mnistTest.hasNext()) {
+         background(0);
+         DataSet input = mnistTest.next();
+         float[] data0 = input.asList().get(0).getFeatures().toFloatVector();
+         for (int y = 0; y < 28; y++) {
+            for (int x = 0; x < 28; x++) {
+               int index = y * 28 + x;
+               fill(data0[index] * 255);
+               rect(x * dotSize, y * dotSize,
+                       (x + 1) * dotSize, (y + 1) * dotSize);
             }
-            float[] array = model.activate(input.getFeatures(), Layer.TrainingMode.TEST).toFloatVector();
-            StringBuilder result = new StringBuilder();
-            int guess = IntStream.range(0, array.length)
-                    .peek(i -> result.append(String.format(" %d=%.3f ", i, array[i])))
-                    .boxed()
-                    .sorted(Comparator.comparing(i -> array[i], Comparator.reverseOrder()))
-                    .findFirst().get();
-            float[] labels = input.getLabels().toFloatVector();
-            int answer = IntStream.range(0, labels.length)
-                    .filter(i -> labels[i] > 0)
-                    .findFirst().getAsInt();
-            fill(guess == answer ? 255 : Color.RED.getRGB());
-            text(answer, dotSize, dotSize * 4);
-            System.out.printf("%s [%s]\n",
-                    result,
-                    "A=" + answer + (answer == guess ? "" : ",G=" + guess));
-        } else {
-            noLoop();
-        }
-    }
+         }
+         float[] array = model.activate(input.getFeatures(), Layer.TrainingMode.TEST).toFloatVector();
+         StringBuilder result = new StringBuilder();
+         int guess = IntStream.range(0, array.length)
+                 .peek(i -> result.append(String.format(" %d=%.3f ", i, array[i])))
+                 .boxed()
+                 .sorted(Comparator.comparing(i -> array[i], Comparator.reverseOrder()))
+                 .findFirst().get();
+         float[] labels = input.getLabels().toFloatVector();
+         int answer = IntStream.range(0, labels.length)
+                 .filter(i -> labels[i] > 0)
+                 .findFirst().getAsInt();
+         fill(guess == answer ? 255 : Color.RED.getRGB());
+         text(answer, dotSize, dotSize * 4);
+         System.out.printf("%s [%s]\n",
+                 result,
+                 "A=" + answer + (answer == guess ? "" : ",G=" + guess));
+      } else {
+         exit();
+      }
+   }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        PApplet.main(TestModel.class.getName());
-    }
+      PApplet.main(TestModel.class.getName());
+   }
 }
